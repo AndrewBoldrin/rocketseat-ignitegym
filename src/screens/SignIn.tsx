d2caafcc49503b3eb566704675c6585da9f4aec7
andrew boldrin <andrewboldrin@hotmail.com>
2023-03-07 20:37:57 -0300

    adicionando button na screen SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import BackgroundImg from "@assets/background.png";
 
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
@@ -34,6 +35,10 @@ export function SignIn() {
         autoCapitalize="none"
       />
       <Input placeholder="Senha" secureTextEntry />
+
+      <Button title="Acessar" />
+
+      <Button title="Criar conta" variant="outline" />
     </VStack>
   );
 }
