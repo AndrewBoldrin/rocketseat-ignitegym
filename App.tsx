3c6222d6454647711aaebe277fc310cf34dfbf59
andrew boldrin <andrewboldrin@hotmail.com>
2023-03-07 18:59:48 -0300

    add componente Loading

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? <Text>Hellow World!</Text> : <View />}
-    </View>
+      {/* {fontsLoaded ? <View /> : <View />} */}
+      <Loading />
+    </NativeBaseProvider>
   );
 }
