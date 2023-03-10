864bd2b5d5b5e820c8466f861e5dc366cb4eb806
andrew boldrin <andrewboldrin@hotmail.com>
2023-03-10 20:39:57 -0300

    listando grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { VStack, FlatList } from "native-base";
 
 export function Home() {
+  const [group, setGroup] = useState(["Costas", "Bíceps", "Tríceps", "ombro"]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={group}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
