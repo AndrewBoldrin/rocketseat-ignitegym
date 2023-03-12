d8d00db865a2a619d5b17cbd8d0d0451c41325ed
andrew boldrin <andrewboldrin@hotmail.com>
2023-03-11 21:11:32 -0300

    exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -19,14 +19,21 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/AndrewBoldrin.png"
+  );
 
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
       allowsEditing: true,
     });
+
+    if (photoSelected.canceled) return;
+
+    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
@@ -45,7 +52,7 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/AndrewBoldrin.png" }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
