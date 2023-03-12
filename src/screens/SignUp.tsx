1754d90a1ef42796a5232462b8942c976badec46
andrew boldrin <andrewboldrin@hotmail.com>
2023-03-12 16:07:03 -0300

    validando senha

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,6 +20,14 @@ type FormDataProps = {
 const signUpSchema = yup.object({
   name: yup.string().required("Informe o nome."),
   email: yup.string().required("Informe o email.").email("E-mail inválido."),
+  password: yup
+    .string()
+    .required("Informe a senha.")
+    .min(6, "A senha deve ter pelo menos 6 digítos."),
+  password_confirm: yup
+    .string()
+    .required("Confime a senha.")
+    .oneOf([yup.ref("password")], "A confirmação da senha não confere."),
 });
 
 export function SignUp() {
